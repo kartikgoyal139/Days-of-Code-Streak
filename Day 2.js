b662bf191d2976ebd195f0d44e0741e779219b69
kartikgoyal139 <91647568+kartikgoyal139@users.noreply.github.com>
2023-01-12 10:35:54 +0530

    Add files via upload

---
+++ Day 2.js
@@ -0,0 +1,23 @@
+// Here we'll see about different types of data types in JS
+
+// String
+var name1 = "Ram";
+var name2 = "Shyam";
+console.log(name1 + " and " + name2 + " are friends.");
+
+//Numbers
+var num1 = 22;
+var num2 = 47;
+var sum = num1+num2;
+console.log("Sum of 22 and 47 is " + sum);
+
+//Exponential Notation
+var e1 = 4e5; //4*10^5
+console.log("4e5 is " + e1);
+
+//Boolean
+let x=false;
+Boolean(x);
+
+  
+  
