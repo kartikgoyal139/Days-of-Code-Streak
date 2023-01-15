a1376234509bd6979594a81a1e85ac346db9f187
kartikgoyal139 <91647568+kartikgoyal139@users.noreply.github.com>
2023-01-15 18:49:42 +0530

    Add files via upload

---
+++ Day 5.js
@@ -0,0 +1,21 @@
+//For Loop
+let text = "";
+
+for (let i = 0; i < 5; i++) {
+  text += "The number is " + i + "<br>";
+}
+
+//While Loop
+let text = "";
+let i = 0;
+while (i < 5) {
+  text += "<br>The number is " + i;
+  i++;
+}
+
+//Do While Loop
+do {
+  text += "<br>The number is " + i;
+  i++;
+}
+while (i < 5);  
