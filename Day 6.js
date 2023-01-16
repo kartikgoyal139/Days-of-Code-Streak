699ca6892413efff711b7ee69db0f8f412c61094
kartikgoyal139 <91647568+kartikgoyal139@users.noreply.github.com>
2023-01-16 22:16:23 +0530

    Add files via upload

--- Day 6.js
+++ Day 6.js
@@ -1,7 +1,9 @@
 //First , we'll define an array
 var names =  ["Ram","Shyam","Geeta","Seeta"];
+
 //No. of elements present in the array can be calculated by arrayName.length function;
 var arrayLength = names.length;
 console.log("The lenght of the array is " + arrayLength);
+
 //In this the nomenclature starts from 0 , not 1
 console.log("The second element in the array is " + names[1]);
