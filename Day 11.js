e07c2da4c39410d046ece126836c8755639578a4
kartikgoyal139 <91647568+kartikgoyal139@users.noreply.github.com>
2023-01-22 20:19:45 +0530

    Add files via upload

---
+++ Day 11.js
@@ -0,0 +1,12 @@
+// Program to check if a string starts with another string
+
+const string = 'Hello World';
+
+const toCheck = 'He';
+
+if(string.startsWith(toCheckS)) {
+    console.log('The string starts with "he".');
+}
+else {
+    console.log('The string does not starts with "he".');
+}
